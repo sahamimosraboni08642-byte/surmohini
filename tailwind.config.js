9760b662ba76d4d3e049757d6f1ff826d6d08e9a
Sraboni Saha <sahamimosraboni08642@gmail.com>
2026-02-16 13:58:36 +0530

    Uploaded on 16.02.2026 again

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A7BFF",
+      }
+    }
+  },
+  plugins: []
+}
